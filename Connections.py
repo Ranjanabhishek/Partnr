84ba85e3d35532f907a34765953ad5eeb66554ca
Ranjanabhishek <ranjanabhishek139@gmail.com>
2024-12-20 14:20:20 +0530

    Uploaded Files

--- Connections.py
+++ Connections.py
@@ -1,23 +1,38 @@
 import json
 
-class Singleton:
+class SingletonMeta(type):
     _instances = {}
-    def __call__(cls):
+
+    def __call__(cls, *args, **kwargs):
         if cls not in cls._instances:
-            cls._instances = super().__call__()
-        return cls._instances
+            cls._instances[cls] = super().__call__(*args, **kwargs)
+        return cls._instances[cls]
 
-class RedisConnection(self, Singleton):
+class RedisConnection(metaclass=SingletonMeta):
+    """
+    Singleton Redis Connection class.
+    """
     def __init__(self):
-        self.connect = {}
-    redis_connect  = Singleton()\
-    return redis_connect
-   
-class DBConnect(self, Singleton):
-    def __init__(self):
-        self.connect = {}
-    db_connect  = Singleton()
-    return db_connect
-   
+        self.connect = {} 
 
-#used singleton pattern here to create a single instace of all connection throught the application
+    def get_connection(self):
+        return self.connect
+
+
+class DBConnect(metaclass=SingletonMeta):
+    """
+     Singleton DB Connection class.
+    """
+
+    def __init__(self):
+        self.connect = {} 
+
+    def get_connection(self):
+        return self.connect
+
+if __name__ == "__main__":
+    redis1 = RedisConnection()
+    redis2 = RedisConnection()
+
+    db1 = DBConnect()
+    db2 = DBConnect()
